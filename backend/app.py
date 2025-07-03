9824a2590a8b54ec12ee15453c263741202420b9
johnwongjething <johnwongjething@gmail.com>
2025-07-03 17:27:53 +0800

    Add full webhook route12456

--- backend/app.py
+++ backend/app.py
@@ -43,10 +43,14 @@ if env_origins and env_origins[0]:
     allowed_origins.extend([origin.strip() for origin in env_origins])
 
 CORS(app, origins=allowed_origins, supports_credentials=True)
-
-
 from payment_webhook import payment_webhook
+from payment_link import payment_link
+
 app.register_blueprint(payment_webhook, url_prefix='/api/webhook')
+app.register_blueprint(payment_link)
+
+
+
 
 @app.route('/api/ping')
 def ping():
